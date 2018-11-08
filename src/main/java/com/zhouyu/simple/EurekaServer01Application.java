f08b419bd130e3e3dde8358648ce340fd0b60c17
zhouyu <13294157562@163.com>
2018-11-08 21:21:42 +0800

    2018-11-8 21:21:35

---
+++ src/main/java/com/zhouyu/simple/EurekaServer01Application.java
@@ -0,0 +1,12 @@
+package com.zhouyu.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer01Application.class, args);
+	}
+}
